1f915e5ce4ba7b45008799b8df1c9b0a851dc7e9
Enes ÇOLAK <enes.colak.business@gmail.com>
2024-10-03 16:57:59 +0300

    JToken First Attempt

---
+++ Token/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Token.Security;
+
+
+namespace Token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Security.Token token = TokenHandler.CreateToken(_configuration);
+            return Ok(token);
+        }
+    }
+}
